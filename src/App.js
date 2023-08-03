96b9ac73828d162ae64dddc113581f89de102de5
klanti117 <klantyislam@yahoo.com>
2023-08-03 14:20:21 -0400

    can see added value in console

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import { useState } from 'react'
 import AddTask from './components/AddTask/AddTask';
 import TasksList from './components/TasksList/TasksList';
 
-const onSubmit = (e) =>{
-  e.preventDefault();
-  console.log('addTasks func called')
-}
+
 
 function App() {
   const [tasksArr,setTasks] = useState([])
+
+  const onSubmit = (e) =>{
+    e.preventDefault();
+    console.log('addTasks func called, passing func as prop')
+    console.log(tasksArr)
+    e.target.reset() //clears field after submission
+  }
+  
+  const onChange = (e) => {
+    setTasks(e.target.value)
+    // console.log(e.target.value) 
+  }
+
   console.log(tasksArr)
   return (
     <div className="App">
       <h1>30 Days of React: Day7-Task List App</h1>
-      {/* <AddTask/> */}
-      <form className='Add-Task'  onSubmit={onSubmit}>
-        <label>
-            <input type="text" name="name" placeholder="Add your tasks here... "/>
-        </label>
-        <input type="submit" value="Submit" />
-        {/* <button onClick={()=>{addTasks()}}>Submit</button> */}
-    </form>
+      <AddTask onChange={onChange} onSubmit={onSubmit}/>
       <TasksList/>
     </div>
   );
--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const AddTask = () => {
+const AddTask = ({ onSubmit, onChange }) => {
   return (
-    <form className='Add-Task'>
+    <form className='Add-Task' onSubmit={onSubmit}>
         <label>
-            <input type="text" name="name" placeholder="Add your tasks here... "/>
+            <input type="text" name="name" placeholder="Add your tasks here... " onChange={onChange}/>
         </label>
         <input type="submit" value="Submit" />
     </form>
